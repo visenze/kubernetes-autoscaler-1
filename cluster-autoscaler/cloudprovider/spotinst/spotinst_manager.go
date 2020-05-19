a38ac4cde0dc4110ef91943a7b9c6fec2a90efd7
Jinyu Pan <jinyu@visenze.com>
2020-05-19 14:01:32 +0800

    add spotinst provider

--- cluster-autoscaler/cloudprovider/spotinst/spotinst_manager.go
+++ cluster-autoscaler/cloudprovider/spotinst/spotinst_manager.go
@@ -36,6 +36,7 @@ import (
 	"github.com/spotinst/spotinst-sdk-go/spotinst/log"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/session"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/util/stringutil"
+	"gopkg.in/gcfg.v1"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
